395cabadc83540a1ed6a83d6a152f02a1ca5661c
chongli <saber4233@gmail.com>
2023-01-08 15:01:41 +0800

    add log

--- src/main/java/top/misec/utils/HttpUtil.java
+++ src/main/java/top/misec/utils/HttpUtil.java
@@ -141,6 +141,7 @@ public class HttpUtil {
             // 关闭资源
             httpResource(httpClient, httpGetResponse);
         }
+        logger.debug("get 请求:{},result:{}",url,resultJson.toString());
         return resultJson;
 
     }
